d5e161513d97920d5c0580dfe11dd3393e9eda89
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-08-26 15:53:36 +0000

    springboot paypal integration

---
+++ src/test/java/com/chidi/payment/PaypalIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chidi.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
